6c1ede445685617b6aa9d452f7088f43be54899c
unknown <phvan2312@gmail.com>
2018-07-13 15:55:58 +0700

    adding preprocessing

---
+++ cikm2018/preprocessing/eng.py
@@ -0,0 +1,190 @@
+"""
+five steps:
+1. Expanding contractions (exp: he's -> he is)
+2. Steaming
+3. Remove number
+4. Normalising
+"""
+
+import re
+from string import digits
+from cucco import Cucco
+
+# List of number terms
+nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+        'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
+        'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred',
+        'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion',
+        'septillion', 'octillion', 'nonillion', 'decillion']
+
+# from https://gist.github.com/nealrs/96342d8231b75cf4bb82
+cList = {
+  "ain't": "am not",
+  "aren't": "are not",
+  "can't": "cannot",
+  "can't've": "cannot have",
+  "'cause": "because",
+  "could've": "could have",
+  "couldn't": "could not",
+  "couldn't've": "could not have",
+  "didn't": "did not",
+  "doesn't": "does not",
+  "don't": "do not",
+  "hadn't": "had not",
+  "hadn't've": "had not have",
+  "hasn't": "has not",
+  "haven't": "have not",
+  "he'd": "he would",
+  "he'd've": "he would have",
+  "he'll": "he will",
+  "he'll've": "he will have",
+  "he's": "he is",
+  "how'd": "how did",
+  "how'd'y": "how do you",
+  "how'll": "how will",
+  "how's": "how is",
+  "I'd": "I would",
+  "I'd've": "I would have",
+  "I'll": "I will",
+  "I'll've": "I will have",
+  "I'm": "I am",
+  "I've": "I have",
+  "isn't": "is not",
+  "it'd": "it had",
+  "it'd've": "it would have",
+  "it'll": "it will",
+  "it'll've": "it will have",
+  "it's": "it is",
+  "let's": "let us",
+  "ma'am": "madam",
+  "mayn't": "may not",
+  "might've": "might have",
+  "mightn't": "might not",
+  "mightn't've": "might not have",
+  "must've": "must have",
+  "mustn't": "must not",
+  "mustn't've": "must not have",
+  "needn't": "need not",
+  "needn't've": "need not have",
+  "o'clock": "of the clock",
+  "oughtn't": "ought not",
+  "oughtn't've": "ought not have",
+  "shan't": "shall not",
+  "sha'n't": "shall not",
+  "shan't've": "shall not have",
+  "she'd": "she would",
+  "she'd've": "she would have",
+  "she'll": "she will",
+  "she'll've": "she will have",
+  "she's": "she is",
+  "should've": "should have",
+  "shouldn't": "should not",
+  "shouldn't've": "should not have",
+  "so've": "so have",
+  "so's": "so is",
+  "that'd": "that would",
+  "that'd've": "that would have",
+  "that's": "that is",
+  "there'd": "there had",
+  "there'd've": "there would have",
+  "there's": "there is",
+  "they'd": "they would",
+  "they'd've": "they would have",
+  "they'll": "they will",
+  "they'll've": "they will have",
+  "they're": "they are",
+  "they've": "they have",
+  "to've": "to have",
+  "wasn't": "was not",
+  "we'd": "we had",
+  "we'd've": "we would have",
+  "we'll": "we will",
+  "we'll've": "we will have",
+  "we're": "we are",
+  "we've": "we have",
+  "weren't": "were not",
+  "what'll": "what will",
+  "what'll've": "what will have",
+  "what're": "what are",
+  "what's": "what is",
+  "what've": "what have",
+  "when's": "when is",
+  "when've": "when have",
+  "where'd": "where did",
+  "where's": "where is",
+  "where've": "where have",
+  "who'll": "who will",
+  "who'll've": "who will have",
+  "who's": "who is",
+  "who've": "who have",
+  "why's": "why is",
+  "why've": "why have",
+  "will've": "will have",
+  "won't": "will not",
+  "won't've": "will not have",
+  "would've": "would have",
+  "wouldn't": "would not",
+  "wouldn't've": "would not have",
+  "y'all": "you all",
+  "y'alls": "you alls",
+  "y'all'd": "you all would",
+  "y'all'd've": "you all would have",
+  "y'all're": "you all are",
+  "y'all've": "you all have",
+  "you'd": "you had",
+  "you'd've": "you would have",
+  "you'll": "you you will",
+  "you'll've": "you you will have",
+  "you're": "you are",
+  "you've": "you have"
+}
+
+c_re = re.compile('(%s)' % '|'.join(cList.keys()))
+
+def expandContractions(text, c_re=c_re):
+    def replace(match):
+        return cList[match.group(0)]
+    return c_re.sub(replace, text)
+
+class EngPreprocessing:
+    def __init__(self):
+        self.norm_eng = Cucco(language='en')
+        self.norm_ops = ['remove_stop_words', 'replace_punctuation', 'remove_extra_whitespaces']
+
+    def process(self, sentences):
+        result = []
+
+        for sentence in sentences:
+            expand_contraction = self.__expand_contraction(sentence.lower())
+            steamming = self.__steaming(expand_contraction)
+            remove_number = self.__remove_number(steamming)
+            normalising  = self.__normalise(remove_number)
+
+            result.append(normalising)
+
+        return result
+
+    def __expand_contraction(self, sentence):
+        return expandContractions(sentence)
+
+    def __steaming(self, sentence):
+        return sentence
+
+    def __remove_number(self, sentence):
+        """
+        Removes all numbers from strings, both alphabetic (in English) and numeric. Intended to be
+        part of a text normalisation process. If the number contains 'and' or commas, these are
+        left behind on the assumption the text will be cleaned further to remove punctuation
+        and stop-words.
+        """
+
+        query = sentence.replace('-', ' ').lower().split(' ')
+        resultwords = [word for word in query if word not in nums]
+        noText = ' '.join(resultwords).encode('utf-8')
+        noNums = noText.translate(None, digits).replace('  ', ' ')
+
+        return noNums
+
+    def __normalise(self, sentence):
+        return self.norm_eng.normalize(text=sentence,normalizations=self.norm_ops)
+
---
+++ cikm2018/preprocessing/spa.py
@@ -0,0 +1,61 @@
+"""
+five steps:
+1. Steaming
+2. Remove number
+3. Normalising
+"""
+
+import re
+from string import digits
+from cucco import Cucco
+
+# List of number terms
+nums = ['cero', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez', 'once',
+        'doce', 'trece', 'catorce', 'quince', 'dieciseis', 'diecisiete', 'dieciocho', 'diecinueve',
+        'veinte', 'veintiuno', 'veintidos', 'veinticinco', 'veintiséis', 'veintisiete', 'veintiocho', 'veintinueve', 'cien',
+        'mil', 'millón', 'billón', 'trillón', 'quadrillion', 'quintillion', 'sextillion',
+        'septillion', 'octillion', 'nonillion', 'decillion']
+
+class SpaPreprocessing:
+    def __init__(self):
+        self.norm_spa = Cucco(language='es')
+        self.norm_ops = ['remove_stop_words', 'replace_punctuation', 'remove_extra_whitespaces']
+
+
+    def process(self, sentences):
+        result = []
+
+        for sentence in sentences:
+            expand_contraction = self.__expand_contraction(sentence.lower())
+            steamming = self.__steaming(expand_contraction)
+            remove_number = self.__remove_number(steamming)
+            normalising = self.__normalise(remove_number)
+
+            result.append(normalising)
+
+        return result
+
+    def __expand_contraction(self, sentence):
+        return sentence
+
+    def __steaming(self, sentence):
+        return sentence
+
+    def __remove_number(self, sentence):
+        """
+        Removes all numbers from strings, both alphabetic (in Spanish) and numeric. Intended to be
+        part of a text normalisation process. If the number contains 'and' or commas, these are
+        left behind on the assumption the text will be cleaned further to remove punctuation
+        and stop-words.
+        """
+
+        query = sentence.replace('-', ' ').split(' ')
+        resultwords = [word for word in query if word not in nums]
+        noText = ' '.join(resultwords).encode('utf-8')
+        noNums = noText.translate(None, digits).replace('  ', ' ')
+
+        return noNums
+
+    def __normalise(self, sentence):
+        return self.norm_spa.normalize(text=sentence, normalizations=self.norm_ops)
+
